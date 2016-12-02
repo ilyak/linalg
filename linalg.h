79ee57530846717b26b95858c864395bf08508b0
Ilya Kaliman <ilya.kaliman@gmail.com>
2016-12-02 13:12:30 -0800

    Avoid namespace pollution.

--- linalg.h
+++ linalg.h
@@ -61,12 +61,15 @@ typedef struct {
 	real zx, zy, zz;
 } m33;
 
+/* Do not pollute global namespace in C++ with preprocessor defines */
+#ifndef __cplusplus
 #define v2idx(v, i) (((real *)&(v))[(i)])
 #define v3idx(v, i) (((real *)&(v))[(i)])
 #define m22idx(m, i, j) (((real *)&(m))[2*(i)+(j)])
 #define m23idx(m, i, j) (((real *)&(m))[3*(i)+(j)])
 #define m32idx(m, i, j) (((real *)&(m))[2*(i)+(j)])
 #define m33idx(m, i, j) (((real *)&(m))[3*(i)+(j)])
+#endif /* __cplusplus */
 
 static inline int
 realeq(real a, real b, real eps)
