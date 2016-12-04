f82b5a32e839e6fe9bbae4b6072e2b97497f2ad5
Ilya Kaliman <ilya.kaliman@gmail.com>
2016-12-04 11:45:40 -0800

    Add some actual code.

--- test.cpp
+++ test.cpp
@@ -1,13 +1,24 @@
 #include "linalg.h"
 
-/* This only tests C++ stuff. Most testing is done in test.c */
+/* This tests C++ stuff. Most testing is done in test.c */
+
+#ifdef LINALG_SINGLE_PRECISION
+#define EPS ((real)1.0e-6)
+#else
+#define EPS ((real)1.0e-16)
+#endif
 
 using namespace linalg;
 
-static int
+static bool
 test01()
 {
-	return (0);
+	m33 a = m33new(1,2,3,2,5,6,3,6,7);
+
+	if (!m33eq(m33m33(a, m33trans(a)), m33m33(m33trans(a), a), EPS))
+		return (true);
+
+	return (false);
 }
 
 int
